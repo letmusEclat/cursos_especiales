8da7bf5607a3fa2bc1049448b855405bff061051
letmusEclat <karen2211855@correo.uis.edu.co>
2024-11-25 21:17:30 -0500

    initial backend project

---
+++ src/main/java/com/notas/registro/RegistroNotasApplication.java
@@ -0,0 +1,13 @@
+package com.notas.registro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notas/registro/RegistroNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notas.registro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
